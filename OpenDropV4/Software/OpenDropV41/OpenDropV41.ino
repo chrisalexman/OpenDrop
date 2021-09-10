c4a1349cd8cd7460e0613de2837c8ec67c55bc92
Chris <christopheralexman@gmail.com>
2021-09-10 13:31:50 -0700

    Add original .ino file

--- OpenDropV4/Software/OpenDropV41/OpenDropV41.ino
+++ OpenDropV4/Software/OpenDropV41/OpenDropV41.ino
@@ -1,7 +1,7 @@
 /*
-    Basic Code to run the OpenDrop V4, Research platform for digital microfluidics
-    Object codes are defined in the OpenDrop.h library
-    Written by Urs Gaudenz from GaudiLabs, 2020
+  Basic Code to run the OpenDrop V4, Research platfrom for digital microfluidics
+  Object codes are defined in the OpenDrop.h library
+  Written by Urs Gaudenz from GaudiLabs, 2020
 */
 
 #include <SPI.h>
@@ -16,8 +16,10 @@
 #include "hardware_def.h"
 
 
-OpenDrop OpenDropDevice = OpenDrop(); 
+
+OpenDrop OpenDropDevice = OpenDrop();
 Drop *myDrop = OpenDropDevice.getDrop();
+
 
 bool FluxCom[16][8];
 int ControlBytesIn[16];
@@ -25,397 +27,158 @@ int ControlBytesOut[24];
 int readbyte;
 
 int JOY_value;
-int joy_x, joy_y;
-int x, y;
-int del_counter = 0;
-int del_counter2 = 0;
+int joy_x,joy_y;
+int x,y;
+int del_counter=0;
+int del_counter2=0;
 
-bool SWITCH_state = true;
-bool SWITCH_state2 = true;
-bool idle = true;
+bool SWITCH_state=true;
+bool SWITCH_state2=true;
+bool idle=true;
 
-bool Magnet1_state = false;
-bool Magnet2_state = false;
+bool Magnet1_state=false;
+bool Magnet2_state=false;
 
-int j = 0;
-  
+int j=0;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
     Serial.begin(115200);
-    
+
     OpenDropDevice.begin("c02");
-    
-    ControlBytesOut[23] = OpenDropDevice.get_ID();
-    
-    OpenDropDevice.set_voltage(240, false, 1000);
-    
+
+    ControlBytesOut[23]=OpenDropDevice.get_ID();
+
+    OpenDropDevice.set_voltage(240,false,1000);
+
     OpenDropDevice.set_Fluxels(FluxCom);
-    
-    pinMode(JOY_pin, INPUT);  
-        
+
+    pinMode(JOY_pin, INPUT);
+
     OpenDropAudio.begin(16000);
     OpenDropAudio.playMe(2);
-    delay(2000);
-    
+    delay (2000);
+
     OpenDropDevice.drive_Fluxels();
     OpenDropDevice.update_Display();
     Serial.println("Welcome to OpenDrop");
-    
-    myDrop->begin(7, 4);
+
+    myDrop->begin(7,4);
     OpenDropDevice.update();
+
 }
 
-// hard-coded loop to activate each electrode in sequence
+
 void loop() {
+    if (Serial.available()>0)             //receive data from App
+    {
+        readbyte=Serial.read();
+        if (x<FluxlPad_width)
+            for (y=0; y < 8; y++) FluxCom[x][y]=(((readbyte) >> (y)) & 0x01);
+        else ControlBytesIn[x-FluxlPad_width]=readbyte;
 
-    if (Serial.available() > 0) {  // receive data from App
-
-        readbyte = Serial.read();
-
-        if (x < FluxlPad_width)
-            for (y = 0; y < 8; y++)
-                FluxCom[x][y] = (((readbyte) >> (y)) & 0x01);
-        else ControlBytesIn[x - FluxlPad_width] = readbyte;
         x++;
-
         digitalWrite(LED_Rx_pin,HIGH);
-        if (x == (FluxlPad_width + 16)) {
-            OpenDropDevice.set_Fluxels(FluxCom);
+        if (x==(FluxlPad_width+16))
+        {OpenDropDevice.set_Fluxels(FluxCom);
             OpenDropDevice.drive_Fluxels();
             OpenDropDevice.update_Display();
 
-            if ((ControlBytesIn[0] & 0x2) && (Magnet1_state == false)) {
-                Magnet1_state = true;
-                OpenDropDevice.set_Magnet(0, HIGH);
+            if ((ControlBytesIn[0]&0x2)&&(Magnet1_state==false)) {
+                Magnet1_state=true;
+                OpenDropDevice.set_Magnet(0,HIGH);
             };
 
-            if (!(ControlBytesIn[0] & 0x2) && (Magnet1_state == true)) {
-                Magnet1_state = false;
-                OpenDropDevice.set_Magnet(0, LOW);
+            if (!(ControlBytesIn[0]&0x2)&&(Magnet1_state==true)) {
+                Magnet1_state=false;
+                OpenDropDevice.set_Magnet(0,LOW);
+            } ;
+
+            if ((ControlBytesIn[0]&0x1)&&(Magnet2_state==false)) {
+                Magnet2_state=true;
+                OpenDropDevice.set_Magnet(1,HIGH);
             };
 
-            if ((ControlBytesIn[0] & 0x1) && (Magnet2_state == false)) {
-                Magnet2_state = true;
-                OpenDropDevice.set_Magnet(1, HIGH);
-            };
+            if (!(ControlBytesIn[0]&0x1)&&(Magnet2_state==true)) {
+                Magnet2_state=false;
+                OpenDropDevice.set_Magnet(1,LOW);
+            } ;
 
-            if (!(ControlBytesIn[0] & 0x1) && (Magnet2_state == true)) {
-                Magnet2_state = false;
-                OpenDropDevice.set_Magnet(1, LOW);
-            };
 
-            for (x = 0; x < 24; x++)
-                Serial.write(ControlBytesOut[x]);
-
-            x = 0;
+            for (x=0; x < 24; x++) Serial.write(ControlBytesOut[x]);
+            x=0;
         };
     }
-    else
-        digitalWrite(LED_Rx_pin, LOW);
-
+    else digitalWrite(LED_Rx_pin,LOW);
     del_counter--;
 
-    if (del_counter < 0) {  // update Display
+    if (del_counter<0) {                 //update Display
         OpenDropDevice.update_Display();
-        del_counter = 1000;
+        del_counter=1000;
     }
 
-    SWITCH_state = digitalRead(SW1_pin);
-    SWITCH_state2 = digitalRead(SW2_pin);
+    SWITCH_state=digitalRead(SW1_pin);
+    SWITCH_state2=digitalRead(SW2_pin);
 
-    if (!SWITCH_state) {  // activate Menu
+    if (!SWITCH_state)                    //activate Menu
+    {
         OpenDropAudio.playMe(1);
         Menu(OpenDropDevice);
         OpenDropDevice.update_Display();
-        del_counter2 = 200;
+        del_counter2=200;
     }
 
-    if (!SWITCH_state2) {  // activate Reservoirs
-        if ((myDrop->position_x() == 15) && (myDrop->position_y() == 3)) {
-            myDrop->begin(14, 1);
-            OpenDropDevice.dispense(1, 1200);
-        }
 
-        if ((myDrop->position_x() == 15) && (myDrop->position_y() == 4)) {
-            myDrop->begin(14, 6);
-            OpenDropDevice.dispense(2, 1200);
-        }
+    if (!SWITCH_state2)                    //activate Reservoirs
+    {
+        if ((myDrop->position_x()==15)&&(myDrop->position_y()==3))
+        {myDrop->begin(14,1);OpenDropDevice.dispense(1,1200);}
+        if ((myDrop->position_x()==15)&&(myDrop->position_y()==4))
+        {myDrop->begin(14,6);OpenDropDevice.dispense(2,1200);}
 
-        if ((myDrop->position_x() == 0) && (myDrop->position_y() == 3)) {
-            myDrop->begin(1, 1);
-            OpenDropDevice.dispense(3, 1200);
-        }
+        if ((myDrop->position_x()==0)&&(myDrop->position_y()==3))
+        {myDrop->begin(1,1);OpenDropDevice.dispense(3,1200);}
+        if ((myDrop->position_x()==0)&&(myDrop->position_y()==4))
+        {myDrop->begin(1,6);OpenDropDevice.dispense(4,1200);}
 
-        if ((myDrop->position_x() == 0) && (myDrop->position_y() == 4)) {
-            myDrop->begin(1, 6);
-            OpenDropDevice.dispense(4, 1200);
-        }
-
-        if ((myDrop->position_x() == 10) && (myDrop->position_y() == 2)) {
-            if (Magnet1_state) {
-                OpenDropDevice.set_Magnet(0, HIGH);
-                Magnet1_state = false;
-            }
-            else {
-                OpenDropDevice.set_Magnet(0, LOW);
-                Magnet1_state = true;
-            }
+        if ((myDrop->position_x()==10)&&(myDrop->position_y()==2))
+        {if (Magnet1_state) {OpenDropDevice.set_Magnet(0,HIGH);Magnet1_state=false;}
+            else {OpenDropDevice.set_Magnet(0,LOW);Magnet1_state=true;}
             while (!digitalRead(SW2_pin));
         }
 
-        if ((myDrop->position_x() == 5) && (myDrop->position_y() == 2)) {
-            if (Magnet2_state) {
-                OpenDropDevice.set_Magnet(1, HIGH);
-                Magnet2_state = false;
-            }
-            else {
-                OpenDropDevice.set_Magnet(1, LOW);
-                Magnet2_state = true;
-            }
+        if ((myDrop->position_x()==5)&&(myDrop->position_y()==2))
+        {if (Magnet2_state) {OpenDropDevice.set_Magnet(1,HIGH);Magnet2_state=false;}
+            else {OpenDropDevice.set_Magnet(1,LOW);Magnet2_state=true;}
             while (!digitalRead(SW2_pin));
         }
+
     }
 
-    // activate each electrode in sequence
-    
-    int row, col;
-    bool rightward = True;
 
-    // dispense droplet from top left reservoir
-    // send droplet to top left corner of grid
-    myDrop->move_up();
-    Serial.println("up");
-    OpenDropDevice.update_Drops();
-    OpenDropDevice.update();
-    
-    for (row = 0; row < 8; row++) {
+    JOY_value = analogRead(JOY_pin);      //navigate using Joystick
 
-        for (col = 0; col < 16; col++) {
-            if rightward {
-                myDrop->move_right();
-                Serial.println("right");
-            }
-            else {
-                myDrop->move_left();
-                Serial.println("left");
-            }
+    if ((JOY_value<950)&(del_counter2==0))
+    {
+        if  (JOY_value<256)
+        {myDrop->move_right();Serial.println("right");}
 
-            OpenDropDevice.update_Drops();
-            OpenDropDevice.update();
-        }
-        
-        rightward = !rightward;
+        if  ((JOY_value>725)&&(JOY_value<895))
+        {myDrop->move_up();    Serial.println("up");}
 
-        if (row != 7) {
-            myDrop->move_down();
-            Serial.println("down");
+        if  ((JOY_value>597)&&(JOY_value<725))
+        {myDrop->move_left();Serial.println("left");}
 
-            OpenDropDevice.update_Drops();
-            OpenDropDevice.update();
-        }
-    }
-    
-    // send droplet back to the output of the top left reservoir
-    for (row = 0; row < 7; row++) {
-        myDrop->move_up();
-        Serial.println("up");
+        if  ((JOY_value>256)&&(JOY_value<597))
+        {myDrop->move_down();Serial.println("down");}
 
         OpenDropDevice.update_Drops();
         OpenDropDevice.update();
-    }
-    
-//    JOY_value = analogRead(JOY_pin);  // navigate using Joystick
-//
-//    if ((JOY_value < 950) & (del_counter2 == 0)) {
-//        if (JOY_value < 256) {
-//            myDrop->move_right();
-//            Serial.println("right");
-//        }
-//        if ((JOY_value > 725) && (JOY_value < 895)) {
-//            myDrop->move_up();
-//            Serial.println("up");
-//        }
-//        if ((JOY_value > 597) && (JOY_value < 725)) {
-//            myDrop->move_left();
-//            Serial.println("left");
-//        }
-//        if ((JOY_value > 256) && (JOY_value < 597)) {
-//            myDrop->move_down();
-//            Serial.println("down");
-//        }
-//
-//        OpenDropDevice.update_Drops();
-//        OpenDropDevice.update();
-//
-//        if (idle) {
-//            del_counter2 = 1800;
-//            idle = false;
-//        }
-//        else
-//            del_counter2 = 400;
-//
-//        del_counter = 1000;
-//    }
-//
-//    if (JOY_value > 950) {
-//        del_counter2 = 0;
-//        idle = true;
-//    }
-//
-//    if (del_counter2 > 0)
-//        del_counter2--;
-}
-
-/*
-void loop() {
-    if (Serial.available() > 0) {  // receive data from App
-
-        readbyte = Serial.read();
-
-        if (x < FluxlPad_width)
-            for (y = 0; y < 8; y++) 
-                FluxCom[x][y] = (((readbyte) >> (y)) & 0x01);
-        else ControlBytesIn[x - FluxlPad_width] = readbyte;
-        x++;
-        
-        digitalWrite(LED_Rx_pin,HIGH); 
-        if (x == (FluxlPad_width + 16)) {
-            OpenDropDevice.set_Fluxels(FluxCom);
-            OpenDropDevice.drive_Fluxels();
-            OpenDropDevice.update_Display();
-            
-            if ((ControlBytesIn[0] & 0x2) && (Magnet1_state == false)) {
-                Magnet1_state = true;
-                OpenDropDevice.set_Magnet(0, HIGH);
-            };
-            
-            if (!(ControlBytesIn[0] & 0x2) && (Magnet1_state == true)) {
-                Magnet1_state = false;
-                OpenDropDevice.set_Magnet(0, LOW);
-            };
-            
-            if ((ControlBytesIn[0] & 0x1) && (Magnet2_state == false)) {
-                Magnet2_state = true;
-                OpenDropDevice.set_Magnet(1, HIGH);
-            };
-
-            if (!(ControlBytesIn[0] & 0x1) && (Magnet2_state == true)) {
-                Magnet2_state = false;
-                OpenDropDevice.set_Magnet(1, LOW);
-            };
-            
-            for (x = 0; x < 24; x++) 
-                Serial.write(ControlBytesOut[x]);
-            
-            x = 0;
-        };
-    } 
-    else 
-        digitalWrite(LED_Rx_pin, LOW);  
-    
-    del_counter--;
-    
-    if (del_counter < 0) {  // update Display
-        OpenDropDevice.update_Display();
-        del_counter = 1000;
-    }
-    
-    SWITCH_state = digitalRead(SW1_pin);
-    SWITCH_state2 = digitalRead(SW2_pin);
-    
-    if (!SWITCH_state) {  // activate Menu
-        OpenDropAudio.playMe(1);
-        Menu(OpenDropDevice);
-        OpenDropDevice.update_Display();
-        del_counter2 = 200;
-    }
-    
-    if (!SWITCH_state2) {  // activate Reservoirs
-        if ((myDrop->position_x() == 15) && (myDrop->position_y() == 3)) {
-            myDrop->begin(14, 1);
-            OpenDropDevice.dispense(1, 1200);
-        }
-
-        if ((myDrop->position_x() == 15) && (myDrop->position_y() == 4)) {
-            myDrop->begin(14, 6);
-            OpenDropDevice.dispense(2, 1200);
-        }
-        
-        if ((myDrop->position_x() == 0) && (myDrop->position_y() == 3)) {
-            myDrop->begin(1, 1);
-            OpenDropDevice.dispense(3, 1200);
-        }
-
-        if ((myDrop->position_x() == 0) && (myDrop->position_y() == 4)) {
-            myDrop->begin(1, 6);
-            OpenDropDevice.dispense(4, 1200);
-        }
-        
-        if ((myDrop->position_x() == 10) && (myDrop->position_y() == 2)) {
-            if (Magnet1_state) {
-                OpenDropDevice.set_Magnet(0, HIGH);
-                Magnet1_state = false;
-            }
-            else {
-                OpenDropDevice.set_Magnet(0, LOW);
-                Magnet1_state = true;
-            }
-            while (!digitalRead(SW2_pin));
-        }
-        
-        if ((myDrop->position_x() == 5) && (myDrop->position_y() == 2)) {
-            if (Magnet2_state) {
-                OpenDropDevice.set_Magnet(1, HIGH);
-                Magnet2_state = false;
-            }
-            else {
-                OpenDropDevice.set_Magnet(1, LOW);
-                Magnet2_state = true;
-            }
-            while (!digitalRead(SW2_pin));
-        }
+        if(idle) {del_counter2=1800;idle=false;} else del_counter2=400;
+        del_counter=1000;
     }
 
-    JOY_value = analogRead(JOY_pin);  // navigate using Joystick
-    
-    if ((JOY_value < 950) & (del_counter2 == 0)) {
-        if (JOY_value < 256) {
-            myDrop->move_right();
-            Serial.println("right");
-        }
-        if ((JOY_value > 725) && (JOY_value < 895)) {
-            myDrop->move_up();
-            Serial.println("up");
-        }
-        if ((JOY_value > 597) && (JOY_value < 725)) {
-            myDrop->move_left();
-            Serial.println("left");
-        }
-        if ((JOY_value > 256) && (JOY_value < 597)) {
-            myDrop->move_down();
-            Serial.println("down");
-        }
-
-        OpenDropDevice.update_Drops();
-        OpenDropDevice.update();
-
-        if (idle) {
-            del_counter2 = 1800;
-            idle = false;
-        }
-        else
-            del_counter2 = 400;
-
-        del_counter = 1000;
-    }  
-    
-    if (JOY_value > 950) {
-        del_counter2 = 0;
-        idle = true;
-    }
-
-    if (del_counter2 > 0)
-        del_counter2--;
+    if (JOY_value>950) {del_counter2=0;idle=true;}
+    if (del_counter2>0) del_counter2--;
 
 } // main loop
-*/
